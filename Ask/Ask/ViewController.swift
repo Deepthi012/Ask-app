09cf8902ba3265e1e355aee8c6c00454bac775a7
Deepthi012 <130980899+Deepthi012@users.noreply.github.com>
2023-05-11 14:56:52 +0530

    Add files via upload

---
+++ Ask/Ask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ask
+//
+//  Created by West Agile Labs on 03/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
